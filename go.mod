49c10fadee917a60f1ced731ba936a4d2245a0c3
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-11-30 20:37:28 +0100

    Implement sliding window log rate

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module teniolafatunmbi/go-limit
 
 go 1.23.1
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
---
+++ pkg/rate_limiter.go
@@ -0,0 +1,64 @@
+package pkg
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+// maybe use a go func that runs in 1 minute intervals to clear logs that fall out of the
+// set sliding `window` interval
+var bucket []time.Time
+
+const WINDOW = 60 * time.Second
+const THRESHOLD = 60
+
+func getIp(str string) string {
+	ip := str
+	if strings.Contains(str, ":") {
+		ip = strings.Split(ip, ":")[0]
+	}
+
+	return ip
+}
+
+func getTotalRequestsInCurrentWindow(bucket []time.Time, window time.Duration) int {
+	noOfRequestsInCurrentWindow := 0
+
+	// start iterating from the end because new request timestamps are appended to the array
+	for i := len(bucket) - 1; i >= 0; i-- {
+		ts := bucket[i]
+		if time.Since(ts) < window {
+			noOfRequestsInCurrentWindow += 1
+		}
+	}
+
+	return noOfRequestsInCurrentWindow
+}
+
+func RateLimiter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RemoteAddr)
+		ipAddress := getIp(r.RemoteAddr)
+		w.Header().Set("x-ip-address", ipAddress)
+
+		requestTimestamp := time.Now()
+
+		// calculate the number of requests in the current <window>
+		noOfRequestsInCurrentWindow := getTotalRequestsInCurrentWindow(bucket, WINDOW)
+
+		fmt.Println("noOfRequestsInCurrentWindow", noOfRequestsInCurrentWindow)
+
+		// if noOfRequestsInCurrentWindow = threshold, discard request, else handle
+		if noOfRequestsInCurrentWindow == THRESHOLD {
+			http.Error(w, "Too many request", http.StatusTooManyRequests)
+			return
+		}
+
+		bucket = append(bucket, requestTimestamp)
+
+		fmt.Println(bucket)
+		next.ServeHTTP(w, r)
+	})
+}
