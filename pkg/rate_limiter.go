46135fbef927b35e775644e00cc67821cd42673a
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-12-18 14:54:35 +0100

    Implement sliding window counter algorithm

--- pkg/rate_limiter.go
+++ pkg/rate_limiter.go
@@ -2,17 +2,29 @@ package pkg
 
 import (
 	"fmt"
+	"math"
 	"net"
 	"net/http"
 	"time"
 )
 
-// maybe use a goroutine that runs in 1 minute intervals to clear logs that fall out of the
-// set sliding `window` interval
-var bucket []time.Time
+type WindowKey string
+type WindowValue struct {
+	key   string
+	count int
+}
+type WindowMap map[WindowKey]*WindowValue
 
-const WINDOW = 60 * time.Second
-const THRESHOLD = 60
+const (
+	CurrentWindowKey  WindowKey = "current"
+	PreviousWindowKey WindowKey = "previous"
+)
+
+var windowMap = make(WindowMap)
+
+// move this into a config file
+const WINDOW_IN_SECONDS = 60 // in seconds
+const THRESHOLD = 40
 
 func getIpFromRemoteAddr(remoteAddr string) (*string, error) {
 	ip, _, err := net.SplitHostPort(remoteAddr)
@@ -24,18 +36,32 @@ func getIpFromRemoteAddr(remoteAddr string) (*string, error) {
 	return &ip, nil
 }
 
-func getTotalRequestsInCurrentWindow(bucket []time.Time, window time.Duration) int {
-	noOfRequestsInCurrentWindow := 0
+func calculateNumberOfRequestsInCurrentWindow(now time.Time, windowMap map[WindowKey]*WindowValue) int {
 
-	// start iterating from the end because new request timestamps are appended to the array
-	for i := len(bucket) - 1; i >= 0; i-- {
-		ts := bucket[i]
-		if time.Since(ts) < window {
-			noOfRequestsInCurrentWindow += 1
-		}
-	}
+	startOfMinute := time.Date(
+		now.Year(), now.Month(), now.Day(),
+		now.Hour(), now.Minute(), 0, 0, now.Location(),
+	)
 
-	return noOfRequestsInCurrentWindow
+	// subtract the time between now and the start of the current minute
+	secondsIntoTheCurrentWindow := int(now.Sub(startOfMinute).Seconds())
+
+	overlapWeight := float64(WINDOW_IN_SECONDS-secondsIntoTheCurrentWindow) / float64(WINDOW_IN_SECONDS)
+
+	// calculate the number of requests in this window
+	numberOfRequestsInCurrentWindow := windowMap[CurrentWindowKey].count + (windowMap[PreviousWindowKey].count * int(math.Round(overlapWeight)))
+
+	return numberOfRequestsInCurrentWindow
+}
+
+func advanceWindow(currentWindow string, windowMap *WindowMap) {
+	(*windowMap)[PreviousWindowKey] = (*windowMap)[CurrentWindowKey]
+	(*windowMap)[CurrentWindowKey] = &WindowValue{key: currentWindow, count: 0}
+}
+
+func initializeNewWindows(windowMap *WindowMap) {
+	(*windowMap)[CurrentWindowKey] = &WindowValue{}
+	(*windowMap)[PreviousWindowKey] = &WindowValue{}
 }
 
 func RateLimiter(next http.Handler) http.Handler {
@@ -47,24 +73,43 @@ func RateLimiter(next http.Handler) http.Handler {
 			return
 		}
 
-		w.Header().Set("x-ip-address", *ipAddress)
+		now := time.Now()
+		currentWindow := fmt.Sprintf("%2d.%2d", now.Hour(), now.Minute())
 
-		requestTimestamp := time.Now()
+		// if the map is empty, add the current window and previous window values
+		_, currentWindowKeyExistsInMap := windowMap[CurrentWindowKey]
+		_, previousWindowKeyExistsInMap := windowMap[PreviousWindowKey]
 
-		// calculate the number of requests in the current <window>
-		noOfRequestsInCurrentWindow := getTotalRequestsInCurrentWindow(bucket, WINDOW)
+		if (currentWindowKeyExistsInMap && previousWindowKeyExistsInMap) == false {
+			initializeNewWindows(&windowMap)
+		}
 
-		fmt.Println("noOfRequestsInCurrentWindow", noOfRequestsInCurrentWindow)
+		// if currentWindow is not the windowMap.current.key,
+		// we're in a new window, so update the current and previous windows
+		if currentWindow != windowMap[CurrentWindowKey].key {
+			advanceWindow(currentWindow, &windowMap)
+		}
 
-		// if noOfRequestsInCurrentWindow = threshold, discard request, else handle
-		if noOfRequestsInCurrentWindow == THRESHOLD {
+		numberOfRequestsInCurrentWindow := calculateNumberOfRequestsInCurrentWindow(now, windowMap)
+
+		fmt.Println("noOfRequestsInCurrentWindow.Sliding", numberOfRequestsInCurrentWindow)
+
+		// if noOfRequestsInCurrentWindow == Zero, discard request, else handle
+		if numberOfRequestsInCurrentWindow == THRESHOLD {
 			http.Error(w, "Too many request", http.StatusTooManyRequests)
 			return
 		}
 
-		bucket = append(bucket, requestTimestamp)
+		windowMap[CurrentWindowKey].count++
 
-		fmt.Println(bucket)
+		fmt.Print("current.window.inferred ", currentWindow, "\n")
+		fmt.Print("current.window.map ", *windowMap[CurrentWindowKey], "\n")
+
+		fmt.Print(*windowMap[CurrentWindowKey], *windowMap[PreviousWindowKey], "\n")
+
+		w.Header().Set("x-ip-address", *ipAddress)
+		w.Header().Set("x-rate-limit-remaining", fmt.Sprintf("%d", THRESHOLD-numberOfRequestsInCurrentWindow))
+
 		next.ServeHTTP(w, r)
 	})
 }
