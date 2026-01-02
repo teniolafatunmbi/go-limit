31209d4076ef6af1d3ba352e5fa989460f60b453
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2026-01-02 17:02:59 +0100

    Cleanup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,10 +14,12 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	PORT := 8080
+	PORT := ":8080"
 
 	r.Use(middleware.Logger)
 	r.Use(pkg.RateLimiter)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
 
 	r.Get("/unlimited", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Unlimited! Let's Go!"))
@@ -25,6 +29,17 @@ func main() {
 		w.Write([]byte("Limited, don't over use me!"))
 	})
 
-	fmt.Printf("Server started at port: %d", PORT)
-	http.ListenAndServe(fmt.Sprintf("localhost:%d", PORT), r)
+	fmt.Printf("Server starting on port %s\n", PORT)
+	server := &http.Server{
+		Addr:         PORT,
+		Handler:      r,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Error starting server: %v\n", err)
+	}
+
 }
